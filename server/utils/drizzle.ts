9aedaf2586b228f04a7fabcbe7b3fbf815d19a57
loumouli <louis.moulin@outlook.fr>
2024-08-04 21:06:02 +0200

    run biome chew

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,5 +1,5 @@
-import { drizzle } from 'drizzle-orm/d1';
 import { sql } from '@vercel/postgres';
+import { drizzle } from 'drizzle-orm/d1';
 export { sql, eq, and, or } from 'drizzle-orm';
 
 import * as schema from '../database/schema';
@@ -7,7 +7,7 @@ import * as schema from '../database/schema';
 export const tables = schema;
 
 export function useDrizzle() {
-    return drizzle(sql, { schema });
+  return drizzle(sql, { schema });
 }
 
 export type Color = typeof schema.colors;
