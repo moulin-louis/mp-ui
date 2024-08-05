06dc48cbdce96c2c03781176f3400b3ba052bf1e
loumouli <louis.moulin@outlook.fr>
2024-08-05 20:08:04 +0200

    refactor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
     clients: {
       component: {
         baseURL: 'http://localhost:3000/api/component',
+        schema: './server/openapi/component/openapi.yaml',
       },
     },
   },
